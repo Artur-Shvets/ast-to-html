16329aa5c445a532c93ecf2ebed370b624a00e19
Artur Shvets <arturshvets477@gmail.com>
2023-12-19 08:39:59 +0200

    Delete src directory

--- src/LevelParser/hooks/index.js
+++
@@ -1,6 +0,0 @@
-export * from './createAllLinks.js';
-export * from './createLink.js';
-export * from './createVertex.js';
-export * from './doDynamicAllLinks.js';
-export * from './getLevels.js';
-export * from './showLevels.js';
--- src/StructureParse/hooks/createSubBlock.js
+++
@@ -1,7 +0,0 @@
-export function createSubBlock(subBlock, mainBlock) {
-  subBlock = document.createElement('div');
-  subBlock.classList.add('sub-block');
-  mainBlock.append(subBlock);
-
-  return subBlock;
-}
